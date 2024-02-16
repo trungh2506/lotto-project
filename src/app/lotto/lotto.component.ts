f9bde39406784692665b42b4ee72c798d9052b89
trungh2506 <trungvipqn12@gmail.com>
2024-02-16 21:05:47 +0700

    cập nhật audio cho chức năng hô lô tô

--- src/app/lotto/lotto.component.ts
+++ src/app/lotto/lotto.component.ts
@@ -1,6 +1,5 @@
 import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
-import { SpeechService } from '../speech.service';
 
 @Component({
   selector: 'app-lotto',
@@ -8,6 +7,9 @@ import { SpeechService } from '../speech.service';
   styleUrls: ['./lotto.component.css'],
 })
 export class LottoComponent {
+  isPlaying: boolean = false; // kiểm tra đang rao lô tô hay chưa
+  audioDuration: any = 0;
+  audio: HTMLAudioElement = new Audio();
   numberInput: any;
   numbers: number[][] = []; //Số hiển thị trên html
 
@@ -15,11 +17,9 @@ export class LottoComponent {
 
   is_lotto_numbers: number[][] = []; // số đã quay
 
+  displayNum: any = 0; // số hiển thị kết quả = randomNum
   randomNum: any = 0; //Số vừa quay
-  constructor(
-    private router: Router,
-    private speechService: SpeechService,
-  ) {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.numbers = this.generateNumbersArray();
@@ -39,34 +39,40 @@ export class LottoComponent {
     return numbersArray;
   }
 
-  randomInLottoNumbers() {
+  async randomInLottoNumbers() {
+    this.isPlaying = true;
     if (this.lotto_numbers.length == 0) {
       alert('Đã rao hết số!');
+      return;
     }
-    const randomRow = Math.floor(Math.random() * this.lotto_numbers.length);
-    const randomCol = Math.floor(
-      Math.random() * this.lotto_numbers[randomRow].length,
-    );
+    while (this.lotto_numbers.length > 0 && this.is_lotto_numbers.length < 90) {
+      const randomRow = Math.floor(Math.random() * this.lotto_numbers.length);
+      const randomCol = Math.floor(
+        Math.random() * this.lotto_numbers[randomRow].length,
+      );
 
-    this.randomNum = this.lotto_numbers[randomRow][randomCol];
+      this.randomNum = this.lotto_numbers[randomRow][randomCol];
+      await this.voiceLotto(this.randomNum);
+      this.displayNum = this.randomNum;
+      // sau khi rao xong thì sẽ hiển thị số lên màn hình
 
-    // Xóa phần tử đã chọn khỏi hàng được chọn
-    this.lotto_numbers[randomRow].splice(randomCol, 1);
+      // Xóa phần tử đã chọn khỏi hàng được chọn
+      this.lotto_numbers[randomRow].splice(randomCol, 1);
 
-    // Nếu bạn muốn xóa cả hàng nếu nó trở thành rỗng
-    if (this.lotto_numbers[randomRow].length === 0) {
-      this.lotto_numbers.splice(randomRow, 1);
+      // Nếu bạn muốn xóa cả hàng nếu nó trở thành rỗng
+      if (this.lotto_numbers[randomRow].length === 0) {
+        this.lotto_numbers.splice(randomRow, 1);
+      }
+
+      // Thêm số đã chọn vào mảng is_lotto_numbers
+      this.is_lotto_numbers.push(this.randomNum);
+
+      //Lưu is lotto numbers vào localStorage
+      localStorage.setItem(
+        'lotto_numbers',
+        JSON.stringify(this.is_lotto_numbers),
+      );
     }
-
-    // Thêm số đã chọn vào mảng is_lotto_numbers
-    this.is_lotto_numbers.push(this.randomNum);
-
-    //Lưu is lotto numbers vào localStorage
-    localStorage.setItem(
-      'lotto_numbers',
-      JSON.stringify(this.is_lotto_numbers),
-    );
-    this.speakText(this.randomNum);
   }
 
   isInLottoNumbers(number: any) {
@@ -98,7 +104,7 @@ export class LottoComponent {
   }
 
   reset() {
-    if (confirm('Bạn có muốn reset ?')) {
+    if (confirm('Bạn có muốn hô lại từ đầu ?')) {
       this.is_lotto_numbers = [];
       this.lotto_numbers = this.generateNumbersArray();
       this.randomNum = 0;
@@ -106,7 +112,52 @@ export class LottoComponent {
     }
   }
 
-  speakText(number: string) {
-    this.speechService.speak(number);
+  //Pause hô lô tô
+  pauseLotto() {
+    this.isPlaying = false;
+    this.audio.pause();
+    alert('Đã dừng!');
+  }
+
+  resumeLotto() {
+    this.isPlaying = true;
+    this.audio.play();
+  }
+
+  // hô lô tô
+  async voiceLotto(number: any): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.audio.src = `../../assets/lotto-audio/raoloto${number}.webm`;
+
+      // Xác định độ dài của audio và sau đó phát âm thanh
+      this.audio.onloadedmetadata = () => {
+        this.audioDuration = this.audio.duration;
+
+        // Làm tròn số
+        this.audioDuration = Math.ceil(this.audioDuration);
+        this.audio.play();
+
+        // Khi phát âm thanh kết thúc, resolve promise
+        this.audio.onended = () => {
+          resolve();
+        };
+      };
+
+      // Xử lý lỗi nếu có
+      this.audio.onerror = (error) => {
+        reject(error);
+      };
+    });
+  }
+
+  //lấy ra số đã ro trước đó
+  previousNumber(): any {
+    let previousNumber =
+      this.is_lotto_numbers[this.is_lotto_numbers.length - 2];
+    if (!previousNumber) {
+      return 0;
+    } else {
+      return previousNumber;
+    }
   }
 }
